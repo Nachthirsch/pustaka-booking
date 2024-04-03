cc8ef7635d206767cce45fd4f8ca8619f12a7df6
Nachthirsch <revengecounter202@gmail.com>
2024-04-03 13:31:15 +0700

    menambahkan data

---
+++ application/controllers/codeigniter.php
@@ -0,0 +1,10 @@
+<?php
+difined('BASEPATH') or exit('No direct script access allowed');
+
+class Bing extends CI_Controller {
+
+    public function index()
+    {
+        $this ->load->view('codeigniter');
+    }
+}
